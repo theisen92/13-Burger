1a59c2ff78823b993e34ddce6c5afd6c3494893a
theisen92 <andrew.peter.theisen@gmail.com>
2020-10-11 10:15:55 -0500

    finishing the styling

--- config/orm.js
+++ config/orm.js
@@ -13,21 +13,15 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
   var arr = [];
 
-  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
@@ -61,10 +55,6 @@ var orm = {
     var queryString = `UPDATE ${table} SET ${objToSql(
       objColVals
     )} WHERE ${condition};`;
-    // queryString += " SET ";
-    // queryString += objToSql(objColVals);
-    // queryString += " WHERE ";
-    // queryString += condition;
 
     console.log(queryString);
     connection.query(queryString, function (err, result) {
@@ -76,9 +66,5 @@ var orm = {
     });
   },
 };
-
-// selectAll();
-// insertOne();
-// updateOne();
 
 module.exports = orm;
