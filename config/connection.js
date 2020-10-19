79e408a54f89c0f503c54883378ec7b5af53a09d
theisen92 <andrew.peter.theisen@gmail.com>
2020-10-19 12:27:38 -0500

    JAWS

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const userCred = require("./config");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_MAUVE_URL)
 } else {
   connection = mysql.createConnection({
     host: userCred.host,
