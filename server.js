fd1347072131eef37547da52a28284fdec4e7580
theisen92 <andrew.peter.theisen@gmail.com>
2020-10-19 21:47:43 -0500

    test

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burger_Controller.js");
+var routes = require("./controllers/burger_controller.js");
 
 app.use(routes);
 
